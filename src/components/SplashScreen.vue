22c6b4cec9d3f6a85c7bc670171c2d6cdf0b489f
David Guzman <davidguzmaninc@gmail.com>
2023-07-29 22:37:16 -0400

    Added the splash screen

---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {ref, onMounted} from 'vue'
+import gsap from "gsap"
+const leftLogo = ref(null);
+const rightLogo = ref(null);
+const displaySplashScreen = ref(true);
+
+onMounted(() => {
+	gsap.to(leftLogo.value, {x: -300, opacity:0, delay: 1, duration: 2});
+	gsap.to(rightLogo.value, {x: 300, opacity:0, delay: 1, duration: 2, onComplete: () => {
+		displaySplashScreen.value = false
+	}});
+})
+</script>
+
+<template>
+    <div v-if="displaySplashScreen" class="SplashScreen">
+        <img ref="leftLogo" src="../assets/left-berry.png" alt="left berry group logo" />
+        <img ref="rightLogo" src="../assets/right-berry.png" alt="right berry group logo" />
+    </div>
+</template>
+
+<style scoped>
+.SplashScreen {
+    height: 100vh;
+    overflow: hidden;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 99;
+}
+</style>
