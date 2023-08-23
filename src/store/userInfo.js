0c58f20801b714480c46155d96a5809276290ccb
David Guzman <davidguzmaninc@gmail.com>
2023-08-23 18:49:17 -0400

    removed email

--- src/store/userInfo.js
+++ src/store/userInfo.js
@@ -2,12 +2,10 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 export const useUserInfo = defineStore('setUserInfo', () => {
     const firstName = ref('');
-    const email = ref('');
     const phoneNumber = ref('');
 
     return {
         firstName,
-        email,
         phoneNumber
     };
 });
