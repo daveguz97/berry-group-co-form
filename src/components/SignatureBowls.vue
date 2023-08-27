6a1149db8ace310428b1931022de4ab2f31d51c0
David Guzman <davidguzmaninc@gmail.com>
2023-08-27 12:56:12 -0400

    Added margin

--- src/components/SignatureBowls.vue
+++ src/components/SignatureBowls.vue
@@ -1,74 +1,87 @@
 <script setup>
 import { onMounted } from 'vue';
 import { gsap } from 'gsap';
-import { useSignatureBowlStore } from '../store/signatureBowls'
-import Form from './Form.vue'
+import { useSignatureBowlStore } from '../store/signatureBowls';
+import Form from './Form.vue';
 import FormButtons from './FormButtons.vue';
 
-const store = useSignatureBowlStore()
+const store = useSignatureBowlStore();
 
 onMounted(() => {
-	gsap.from('.bowl-container', { duration: 1, opacity: 0 }).duration(1);
-})
+    gsap.from('.bowl-container', { duration: 1, opacity: 0 }).duration(1);
+});
 </script>
 
 <template>
     <Form>
         <div class="container">
             <div class="bowl-container" v-for="bowl in store.signatureBowls" :key="bowl.id">
-                <label :for="bowl.name">&nbsp;{{ bowl.name }}</label>
-				<div>
-                            <button
-                                class="ingredients-button"
-                                @click.prevent="store.toggleCollapse(bowl)"
-                            >
-                                {{
-                                    bowl.collapsed
-                                        ? '&rarr; show ingredients'
-                                        : '&darr; Hide Ingredients'
-                                }}
-                            </button>
-                        </div>
-						<ul v-if="!bowl.collapsed">
-                            <li v-for="ingredient in bowl.toppings" :key="ingredient">
-                                {{ ingredient }}
-                            </li>
-                        </ul>
-                <div class="size-container">
+                <div class="form-group">
+                    <label :for="bowl.name">&nbsp;{{ bowl.name }}</label>
+                    <small class="base" :for="bowl.base">Base: {{ bowl.base }}</small>
+                </div>
+                <div class="form-group">
+                    <button class="ingredients-button" @click.prevent="store.toggleCollapse(bowl)">
+                        {{ bowl.collapsed ? '&rarr; show ingredients' : '&darr; Hide Ingredients' }}
+                    </button>
+                </div>
+                <ul v-if="!bowl.collapsed">
+                    <li v-for="ingredient in bowl.toppings" :key="ingredient">
+                        {{ ingredient }}
+                    </li>
+                </ul>
+                <div class="size-container form-group">
                     <template v-for="size in bowl.sizes" :key="size">
-                            <input v-model="size.checked" name="price" type="checkbox" />
-                            <label class="size" for="price">
-                                {{ size.size }}
-                            </label>
+                        <input v-model="size.checked" name="price" type="checkbox" />
+                        <label class="size" for="price">
+                            {{ size.size }}
+                        </label>
                     </template>
                 </div>
             </div>
         </div>
-		<FormButtons />
-		<p class="error-message" v-if="store.showErrorMessage">Please select a bowl.</p>
+        <FormButtons />
+        <p class="error-message" v-if="store.showErrorMessage">Please select a bowl.</p>
     </Form>
 </template>
 
 <style scoped>
 .container {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+}
+.form-group {
+	margin: .3rem 0;
 }
 .bowl-container {
-	margin: .5rem 0;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+    margin: 0.5rem 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
 .size-container {
-	display: flex;
-	justify-content: center;
+    display: flex;
+    justify-content: center;
 }
 
 .size-container input {
-	margin: 0 1rem
+    margin: 0 1rem;
+}
+
+.base {
+	font-weight: 400;
+	font-size: 12px;
+}
+
+.form-group:first-child {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.form-group:first-child label {
+	margin: .3rem 0;
 }
 </style>
